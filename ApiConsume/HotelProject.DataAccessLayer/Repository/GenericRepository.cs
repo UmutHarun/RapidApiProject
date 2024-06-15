ac8f8618a5846f61f8ac8b5933a902ea2d9f4edf
harunC <harun123ors@gmail.com>
2024-06-15 14:53:33 +0300

    EntityFrameworks added

---
+++ ApiConsume/HotelProject.DataAccessLayer/Repository/GenericRepository.cs
@@ -0,0 +1,49 @@
+﻿using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.DataAccessLayer.Repository
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        private readonly Context _context;
+        private Context context;
+
+        public GenericRepository(Context context)
+        {
+            this.context = context;
+        }
+
+        public void Delete(T entity)
+        {
+            _context.Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public List<T> GetAll()
+        {
+           return _context.Set<T>().ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return _context.Set<T>().Find(id);
+        }
+
+        public void Insert(T entity)
+        {
+            _context.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            _context.Update(entity);    
+            _context.SaveChanges();
+        }
+    }
+}
