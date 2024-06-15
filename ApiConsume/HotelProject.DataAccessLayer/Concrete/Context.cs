f9d293b92a953b8ef815778a11154b39635c39e2
harunC <harun123ors@gmail.com>
2024-06-15 14:02:53 +0300

    abstract classes added

---
+++ ApiConsume/HotelProject.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,25 @@
+﻿using HotelProject.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.DataAccessLayer.Concrete
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=DESKTOP-GT2GK83;Initial Catalog=HotelRapidApiDb;Integrated Security=True;TrustServerCertificate=True");
+        }
+
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Staff> Staffs { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Subscribe> Subscribes { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+    }
+}
